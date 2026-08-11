0a5edc31d902e98f5b78ca130ece8576e0f0fe0d
Naina <panwarnaina134@gmail.com>
2026-08-11 22:00:33 +0530

    Time: 1 ms (45.65%), Space: 55.6 MB (41.3%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,17 @@
+function missingInteger(nums) {
+    let sum = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] === nums[i - 1] + 1) {
+            sum += nums[i];
+        } else {
+            break;
+        }
+    }
+    
+    const set = new Set(nums);
+    while (set.has(sum)) {
+        sum++;
+    }
+    
+    return sum;
+}
